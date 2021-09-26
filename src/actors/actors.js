6032b5d73e72cc39f4bdcaad0d83439ee75ef25d
mahedee007 <mahedee007@gmail.com>
2021-09-26 15:27:38 +0600

    first commit

---
+++ src/actors/actors.js
@@ -0,0 +1,203 @@
+const actors = [
+    {
+        id: 1001,
+        name : 'Tom Hanks',
+        gender : 'male',
+        age : 65,
+        bestmovie : 'Forrest Gump',
+        img : 'https://im.ge/i/T8qa3x',
+        salary : 222222, 
+
+    },
+    {
+        id : 1002,
+        name : 'Leonardo DiCaprio',
+        gender : 'male',
+        age : 46,
+        bestmovie : 'The Blood Diamond',
+        img : 'https://i.im.ge/2021/09/26/T8qwYX.jpg',
+        salary : 450000, 
+
+    },
+    {
+        id : 1003,
+        name : 'Julia Roberts',
+        gender : 'female',
+        age : 53,
+        bestmovie : 'Preety women',
+        img : 'https://i.im.ge/2021/09/26/T8qkZT.jpg',
+        salary : 150000, 
+
+    },
+    {
+        id : 1004,
+        name : 'Brad Pitt',
+        gender : 'male',
+        age : 57,
+        bestmovie : 'Troy',
+        img : 'https://i.im.ge/2021/09/26/T8sM1S.jpg',
+        salary : 580000, 
+
+    },
+    {
+        id : 1005,
+        name : 'Jhony Depp',
+        gender : 'male',
+        age : 58,
+        bestmovie : 'The Tourist',
+        img : 'https://i.im.ge/2021/09/26/T8s7XC.jpg',
+        salary : 350000, 
+
+    },
+    {
+        id : 1006,
+        name : 'Angelina Jolie',
+        gender : 'female',
+        age : 46,
+        bestmovie : 'Salt',
+        img : 'https://i.im.ge/2021/09/26/T8sjwW.jpg',
+        salary : 190000, 
+
+    },
+    {
+        id : 1007,
+        name : 'Drew Barrymore',
+        gender : 'female',
+        age : 46,
+        bestmovie : 'The Due Date',
+        img : 'https://i.im.ge/2021/09/26/T8sA7x.jpg',
+        salary : 200000, 
+
+    },
+    {
+        id : 1008,
+        name : 'Salman Khan',
+        gender : 'male',
+        age : 55,
+        bestmovie : 'Bajrangi Bhaijan',
+        img : 'https://i.im.ge/2021/09/26/T8sKDX.jpg',
+        salary : 367808, 
+
+    },
+    {
+        id : 1009,
+        name : 'Adam Sandler',
+        gender : 'male',
+        age : 55,
+        bestmovie : 'Grown Ups',
+        img : 'https://i.im.ge/2021/09/26/T8a3xP.jpg',
+        salary : 500000, 
+
+    },
+    {
+        id : 1010,
+        name : 'Jennifer Aniston',
+        gender : 'female',
+        age : 52,
+        bestmovie : 'We are the Miller',
+        img : 'https://i.im.ge/2021/09/26/T8anky.jpg',
+        salary : 230000, 
+
+    },
+    {
+        id : 10011,
+        name : 'Ranbir Kapoor ',
+        gender : 'male',
+        age : 38,
+        bestmovie : 'Sanju',
+        img : 'https://i.im.ge/2021/09/26/T87rq8.jpg',
+        salary : 260000, 
+
+    },
+    {
+        id : 1012,
+        name : 'Will Smith',
+        gender : 'male',
+        age : 53,
+        bestmovie : 'Persuit Of Happiness',
+        img : 'https://i.im.ge/2021/09/26/T8774P.jpg',
+        salary : 578000, 
+
+    },
+    {
+        id : 1013,
+        name : 'Madhuri Dixit',
+        gender : 'male',
+        age : 54,
+        bestmovie : 'Hum Apke Hain Kaun',
+        img : 'https://i.im.ge/2021/09/26/T87tya.jpg',
+        salary : 380000, 
+
+    },
+    {
+        id : 1014,
+        name : 'Ananta Jalil',
+        gender : 'male',
+        age : 50,
+        bestmovie : 'What is love',
+        img : 'https://i.im.ge/2021/09/26/T87Wy8.jpg',
+        salary : 500000, 
+
+    },
+    {
+        id : 1015,
+        name : 'Emraan Hashmi',
+        gender : 'male',
+        age : 42,
+        bestmovie : 'Murder',
+        img : 'https://i.im.ge/2021/09/26/T8Drbc.jpg',
+        salary : 230000, 
+
+    },
+    {
+        id : 1016,
+        name : 'Arnold Schwarzenegger',
+        gender : 'male',
+        age : 74,
+        bestmovie : 'Terminator',
+        img : 'https://i.im.ge/2021/09/26/T8DiCX.jpg',
+        salary : 590000, 
+
+    },
+    {
+        id : 1017,
+        name : 'Salma Hayek',
+        gender : 'female',
+        age : 55,
+        bestmovie : 'Desperado',
+        img : 'https://i.im.ge/2021/09/26/T8DCCP.jpg',
+        salary : 120000, 
+
+    },
+    {
+        id : 1018,
+        name : 'Courteney Cox',
+        gender : 'female',
+        age : 57,
+        bestmovie : 'friends',
+        img : 'https://i.im.ge/2021/09/26/T8DzSa.jpg',
+        salary : 200000, 
+
+    },
+    {
+        id : 1019,
+        name : 'Matthew Perry',
+        gender : 'male',
+        age : 52,
+        bestmovie : '17 Again',
+        img : 'https://i.im.ge/2021/09/26/T8IXNh.jpg',
+        salary : 300000, 
+
+    },
+    {
+        id : 1020,
+        name : 'Lisa Kudrow',
+        gender : 'female',
+        age : 58,
+        bestmovie : 'Kabluey ',
+        img : 'https://i.im.ge/2021/09/26/T80Rgy.jpg',
+        salary : 290000, 
+
+    },
+    
+]
